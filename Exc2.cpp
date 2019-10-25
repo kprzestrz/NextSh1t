69535628ecc3e56a6f8bdaba3c71d5790cab5e86
Jakub Przestrzelski <s21677@pjwstk.edu.pl>
2019-10-25 16:20:18 +0200

    Exc4fromProject

--- Exc2.cpp
+++ Exc2.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <cstdlib>
 
 int main()
 {
     std::cout << "Hello World!\n";
 	std::cout << "Pieprzyc!";
 	//nie ogarniam
+	//zad 4:
+	std::cout << "Wprowadz liczbe:";
+	int liczba1;
+	std::cin >> liczba1;
+	std::cout << liczba1 + 1;
+	std::cout << std::endl;
+	system("cls");
 }
