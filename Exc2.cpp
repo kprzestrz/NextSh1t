ec0e839957ae7e3474c383ac836dac6d291ced9c
Szymon Jakóbiak <s20574@pjwstk.edu.pl>
2019-10-25 16:31:59 +0200

    PierwszeCouty

--- Exc2.cpp
+++ Exc2.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
 int main()
+using namespace std;
 {
-    std::cout << "Hello World!\n";
-	std::cout << "Pieprzyc!";
-	//nie ogarniam
+	int a, b, c, d, e;
+	cout << "WLASNIE JESTES W RZYMIE, STOISZ POD BIURKIEM I MASZ DLUGOPIS\n";
+	cout << "Lista kandydatów: \n";
+	cout << "1. Kapitan Bomba \n";
+	cin >> a;
+	cout << "2. Bonus BGC \n";
+	cin >> b;
+	cout << "3. Lech Roch Pawlak \n";
+	cin >> c;
+	cout << "4. Siostry Godlewskie \n";
+	cin >> d;
+	cout << "5. Czarus z chlopakow do wziecia \n";
+	cin >> e;
+
+
 }
